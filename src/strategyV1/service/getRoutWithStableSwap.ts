8608cc1838a691c54a83b70d2b52b96bcc741421
chefjackson <116779127+chefjackson@users.noreply.github.com>
2022-11-01 18:34:33 +0800

    refactor: use currency amount compare as safe compare

--- src/strategyV1/service/getRoutWithStableSwap.ts
+++ src/strategyV1/service/getRoutWithStableSwap.ts
@@ -26,72 +26,73 @@ export const getRoutWithStableSwap = async (
   for (let i = 0; i < bestRoutePairs.length; i++) {
     for (let j = 0; j < stableSwapPairs.length; j++) {
       const pair: Pair = stableSwapPairs[j];
-      if (isPairsEquals(bestRoutePairs[i], pair)) {
-        console.log(
-          `pair found index: ${i}, lastStableSwapIndex: ${lastStableSwapIndex}`
-        );
-        combinePairs.push(pair);
-
-        let tokenIn: SdkToken = null,
-          tokenOut: SdkToken = null;
-        if (i > 0) {
-          //Check previous pair output token
-          tokenIn = findTokenByAddress(bestRoutePairs[i - 1], pair.token0.id);
-          tokenOut = findTokenByAddress(bestRoutePairs[i - 1], pair.token1.id);
-
-          //Calculate trade in case we didn't have StableSwap pair before, or when index between StableSwap pairs > 1
-          if (
-            lastStableSwapIndex === -1 ||
-            (lastStableSwapIndex !== -1 && i - lastStableSwapIndex > 1)
-          ) {
-            //Calculate route before we found StableSwap pair
-            const fromIndex =
-              lastStableSwapIndex === -1 ? 0 : lastStableSwapIndex + 1;
-            const nextPairSameToken0 = findTokenByAddress(
-              bestRoutePairs[fromIndex + 1],
-              bestRoutePairs[fromIndex].token0.address
-            );
-
-            const tokenFrom = nextPairSameToken0
-              ? bestRoutePairs[fromIndex].token1
-              : bestRoutePairs[fromIndex].token0;
-
-            outputAmountWei = await getTradeOutputAmountWei(
-              bestRoutePairs,
-              tokenFrom,
-              tokenIn || tokenOut,
-              outputAmountWei,
-              network
-            );
-            console.log("trade outputAmountWei", outputAmountWei);
-          }
-        }
-
-        //When StableSwap is first pair
-        if (tokenIn === null && tokenOut === null) {
-          console.log(
-            `StableSwap firstPair bestRoutePairs[i] ${bestRoutePairs[i].token0.symbol}-${bestRoutePairs[i].token1.symbol}`
-          );
-          outputAmountWei = await getPairPriceStableSwap(
-            pair.id,
-            equalsIgnoreCase(bestRoutePairs[i].token0.address, pair.token0.id)
-              ? outputAmountWei
-              : null,
-            equalsIgnoreCase(bestRoutePairs[i].token0.address, pair.token1.id)
-              ? outputAmountWei
-              : null
-          );
-          //All others cases
-        } else {
-          outputAmountWei = await getPairPriceStableSwap(
-            pair.id,
-            tokenOut ? outputAmountWei : null,
-            tokenIn ? outputAmountWei : null
-          );
-        }
-
-        lastStableSwapIndex = i;
+      if (!isPairsEquals(bestRoutePairs[i], pair)) {
+        continue;
       }
+      console.log(
+        `pair found index: ${i}, lastStableSwapIndex: ${lastStableSwapIndex}`
+      );
+      combinePairs.push(pair);
+
+      let tokenIn: SdkToken = null,
+        tokenOut: SdkToken = null;
+      if (i > 0) {
+        //Check previous pair output token
+        tokenIn = findTokenByAddress(bestRoutePairs[i - 1], pair.token0.id);
+        tokenOut = findTokenByAddress(bestRoutePairs[i - 1], pair.token1.id);
+
+        //Calculate trade in case we didn't have StableSwap pair before, or when index between StableSwap pairs > 1
+        if (
+          lastStableSwapIndex === -1 ||
+          (lastStableSwapIndex !== -1 && i - lastStableSwapIndex > 1)
+        ) {
+          //Calculate route before we found StableSwap pair
+          const fromIndex =
+            lastStableSwapIndex === -1 ? 0 : lastStableSwapIndex + 1;
+          const nextPairSameToken0 = findTokenByAddress(
+            bestRoutePairs[fromIndex + 1],
+            bestRoutePairs[fromIndex].token0.address
+          );
+
+          const tokenFrom = nextPairSameToken0
+            ? bestRoutePairs[fromIndex].token1
+            : bestRoutePairs[fromIndex].token0;
+
+          outputAmountWei = await getTradeOutputAmountWei(
+            bestRoutePairs,
+            tokenFrom,
+            tokenIn || tokenOut,
+            outputAmountWei,
+            network
+          );
+          console.log("trade outputAmountWei", outputAmountWei);
+        }
+      }
+
+      //When StableSwap is first pair
+      if (tokenIn === null && tokenOut === null) {
+        console.log(
+          `StableSwap firstPair bestRoutePairs[i] ${bestRoutePairs[i].token0.symbol}-${bestRoutePairs[i].token1.symbol}`
+        );
+        outputAmountWei = await getPairPriceStableSwap(
+          pair.id,
+          equalsIgnoreCase(bestRoutePairs[i].token0.address, pair.token0.id)
+            ? outputAmountWei
+            : null,
+          equalsIgnoreCase(bestRoutePairs[i].token0.address, pair.token1.id)
+            ? outputAmountWei
+            : null
+        );
+        //All others cases
+      } else {
+        outputAmountWei = await getPairPriceStableSwap(
+          pair.id,
+          tokenOut ? outputAmountWei : null,
+          tokenIn ? outputAmountWei : null
+        );
+      }
+
+      lastStableSwapIndex = i;
     }
     if (lastStableSwapIndex !== i) {
       combinePairs.push(fromSdkPair(bestRoutePairs[i]));
